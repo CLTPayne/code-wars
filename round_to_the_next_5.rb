eeaed635453c0fcd5ca3494df10a4b3d028e2ba5
Charlotte Payne <35195024+CLTPayne@users.noreply.github.com>
2018-05-08 09:24:30 +0100

    Create round_to_the_next_5.rb

---
+++ round_to_the_next_5.rb
@@ -0,0 +1,34 @@
+=begin
+Level 7 Kyu Kata - Ruby 
+
+Given an integer as input, can you round it to the next (meaning, "higher") 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.
+=end 
+
+def round_to_next_5(n)
+  if n % 5 == 0
+    return n
+  elsif n % 5 > 0
+    return n + (5 - (n % 5))
+  end
+end
+
+# Solution - check to see if number already is divisable by 5. 
+# If / elsif to find remainder (modulo) of n divided by 5. 
+# Deduct this remainder from 5
+# Add this to n and return the outcome. 
